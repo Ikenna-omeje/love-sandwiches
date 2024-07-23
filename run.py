56f83544d33a073cdb83624561d1300eecb59e41
Ikenna Omeje <karl.ikenna@outlook.com>
2024-07-23 21:42:43 +0000

    add validation to check if the values are 6 in number

--- run.py
+++ run.py
@@ -12,8 +12,34 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('love-sandwiches') 
 
-sales = SHEET.worksheet('sales')
 
-data = sales.get_all_values()
+def get_sales_data():
+    """
+    Get sales figures input from the user.
+    """
+    print("Please enter sales data from the last market.")
+    print("Data should be six numbers, separated by commas.")
+    print("Example: 10,20,30,40,50,60\n")
 
-print(data)
+    data_str = input("Enter your data here: ")
+    
+    sales_data = data_str.split(",")
+    validate_data(sales_data)
+
+def validate_data(values):
+    """
+    inside try, convert all strings to interger,
+    raise valueerror if strings cannot be converted into integer, 
+    or if there arent exactly 6 values.
+    """
+    try:
+        if len(values) != 6:
+           raise ValueError(
+                f"exactly 6 values required, you provided {len(values)}"
+           )
+    except ValueError as e: 
+        print(f"invalid data: {e}, please try again.\n")
+
+
+
+get_sales_data()
