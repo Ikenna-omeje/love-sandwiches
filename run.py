761f3a0ec13f4ac55f4e7fe69917ec516baa2ba4
Ikenna Omeje <karl.ikenna@outlook.com>
2024-07-24 22:54:53 +0000

    add sales data to the worksheet

--- run.py
+++ run.py
@@ -17,30 +17,50 @@ def get_sales_data():
     """
     Get sales figures input from the user.
     """
-    print("Please enter sales data from the last market.")
-    print("Data should be six numbers, separated by commas.")
-    print("Example: 10,20,30,40,50,60\n")
+    while True:
 
-    data_str = input("Enter your data here: ")
-    
-    sales_data = data_str.split(",")
-    validate_data(sales_data)
+        print("Please enter sales data from the last market.")
+        print("Data should be six numbers, separated by commas.")
+        print("Example: 10,20,30,40,50,60\n")
+
+        data_str = input("Enter your data here: ")
+
+        sales_data = data_str.split(",")
+        
+        
+        if validate_data(sales_data):
+            print("Data is valid!")
+            break
+
+    return sales_data       
 
 def validate_data(values):
     """
     inside try, convert all strings to interger,
-    raise valueerror if strings cannot be converted into integer, 
+    raise valueError if strings cannot be converted into integer, 
     or if there arent exactly 6 values.
     """
-    print(values)
     try:
         [int(value) for value in values]
         if len(values) != 6:
            raise ValueError(
-                f"exactly 6 values required, you provided {len(values)}"
+                f"Exactly 6 values required, you provided {len(values)}"
            )
     except ValueError as e: 
         print(f"invalid data: {e}, please try again.\n")
+        return False
 
+    return True
 
-get_sales_data()
+def update_sales_Worksheet(data):
+    """
+    update worksheet data, add new row in data data list provided.
+    """
+    print("updating sales Worksheet...\n")
+    sales_worksheet = SHEET.worksheet("sales")
+    sales_worksheet.append_row(data)
+    print("sales_worksheet updated succesfully.\n")
+data = get_sales_data()
+
+sales_data = [int(num) for num in data]
+update_sales_Worksheet(sales_data)
